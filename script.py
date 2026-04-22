3980a7b757571e1fa034b7e98f45a3dc18528aaa
nurslev <nursultanbakinov73@gmail.com>
2026-04-22 11:19:36 +0600

    Add word frequency count and max occurrence print
    Count occurrences of words in a string and print the most frequent one.

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
+text = "cat dog cat"
 
+words = text.split()
+
+count = {}
+
+for word in words:
+    if word in count:
+        count[word] += 1
+    else:
+        count[word] = 1
+
+print(max(count, key=count.get))
